5bb2d4b4579c63dfe40de925d0006443e901fd5f
John Arboleda <JohnJack0126@gmail.com>
2021-04-21 12:54:03 -0500

    SELECT in SELECT

---
+++ select_in_select.sql
@@ -0,0 +1,60 @@
+-- SELECT within SELECT Tutorial
+
+-- 1. Bigger than Russia
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia');
+
+-- 2. Richer than UK
+SELECT name
+  FROM world
+  WHERE continent = 'Europe' AND gdp/population >
+  (SELECT gdp/population
+  FROM world
+  WHERE name = 'United Kingdom');
+
+-- 3. Neighbours of Argentina and Australia
+SELECT name, continent
+  FROM world
+  WHERE continent IN (SELECT continent FROM world WHERE name IN ('Argentina', 'Australia'))
+ORDER BY name;
+
+-- 4. Between Canada and Poland
+SELECT name
+  FROM world
+  WHERE population > (
+    SELECT population FROM world WHERE name = 'Canada') AND
+  population < (SELECT population FROM world WHERE name = 'Poland');
+
+-- 5. Percentages of Germany
+SELECT name, concat(round(population / (SELECT population
+                                          FROM world
+                                         WHERE name = 'Germany') * 100), '%') AS percentage
+  FROM world
+  WHERE continent = 'Europe';
+
+-- 6. Bigger than every country in Europe
+SELECT name
+  FROM world
+  WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp > 0)
+
+-- 7. Largest in each continent
+SELECT continent, name, area FROM world w1
+  WHERE area >= ALL
+    (SELECT area FROM world w2
+        WHERE w2.continent = w1.continent
+          AND area > 0)
+
+-- 8. First country of each continent (alphabetically)
+SELECT continent, name FROM world x WHERE name = all(SELECT TOP(1) name FROM world y WHERE x.continent = y.continent);
+
+-- 9. Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+SELECT name, continent, population FROM world w1
+WHERE 25000000 >= ALL(SELECT population FROM world w2 WHERE w1.continent = w2.continent)
+
+-- 10. 
+SELECT name, continent FROM world x
+  WHERE population >= ALL
+    (SELECT population * 3 FROM world y
+        WHERE y.continent=x.continent AND x.name <> y.name);
